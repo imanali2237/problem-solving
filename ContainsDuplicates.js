beac1d51fd845d38f79d2f78079854dc95bcf934
imanali2237 <imanali341012@gmail.com>
2025-04-09 00:38:29 +0500

    Contains Duplicate Problems Done

---
+++ ContainsDuplicates.js
@@ -0,0 +1,18 @@
+function checkOontainDuplicate(array){
+    const hashSet=new Set()
+    for(const element of array){
+        if(hashSet.has(element)) return true
+
+       
+         else   hashSet.add(element)
+  
+        
+
+
+    }
+    return false
+
+}
+
+ let nums =[1, 2, 3, 4]
+ console.log(checkOontainDuplicate(nums))
